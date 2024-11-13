20304b100140980f3842983532157c1814e71cee
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-11-13 12:02:36 +0300

    structure of project

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,35 @@
+from dotenv import load_dotenv
 import allure
+import os
 import pytest
 from selene import browser
 
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope="function", autouse=True)
 def driver_configuration():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = "https://spinningist.ru"
-
     yield
     browser.quit()
+
+# @pytest.fixture(scope="function", autouse=True)
+# def auth_driver_configuration():
+#     browser.config.window_width = 1920
+#     browser.config.window_height = 1080
+#     browser.config.base_url = "https://spinningist.ru"
+#     token = os.getenv('TOKEN')
+#     cookie = {
+#         "name": "session_",
+#         "value": token
+#     }
+#     browser.open('/')
+#     browser.driver.add_cookie(cookie)
+#
+#
+#     yield
+#     browser.quit()
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,3 +1,5 @@
+import os
+
 from selene import browser, be, have
 import time
 from spinningist_project.pages.registration_page import RegistrationPage
@@ -5,7 +7,7 @@ from spinningist_project.pages.registration_page import RegistrationPage
 
 def test_open_page():
     browser.open('/')
-    browser.element('[class="hidden-sm hidden-xs b-cmall-members-small_menu_login__item-link-name"]').click()
+    # browser.element('[class="hidden-sm hidden-xs b-cmall-members-small_menu_login__item-link-name"]').click()
 
     time.sleep(2)
 
@@ -13,6 +15,11 @@ def test_open_page():
 def test_login():
     registration_page = RegistrationPage()
     registration_page.open()
-    registration_page.fill_login('test_login')
-    registration_page.fill_password('111111test')
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    registration_page.fill_login(login)
+    registration_page.fill_password(password)
     registration_page.submit()
+    time.sleep(3)
+
+
