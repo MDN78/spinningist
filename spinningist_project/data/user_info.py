5fca9f150677cf8fb5abd12bb4c9c1514dd55487
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-11-13 16:31:42 +0300

    update project

---
+++ spinningist_project/data/user_info.py
@@ -0,0 +1,21 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class User:
+    name: str
+    email: str
+    phone: str
+    street: str
+    house: str
+    additional_information: str
+
+
+Alex = User(
+    name='Evlampy',
+    email='test@yandex.ru',
+    phone='9253343434',
+    street='Lermontova',
+    house='12',
+    additional_information="Delivery after 12 o'clock",
+)
